6d87a84d2303a3ffe4ba1513ba47c86020dade5b
hrnugr0000 <ugur.harun@yandex.com>
2020-01-31 20:05:24 +0300

    Updates

---
+++ precache-manifest.7c0327a9b65a8bab58b9ccd4778b9de3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13d1a43d7ff97bc14a6d413c5ec0368",
+    "url": "/form-app/index.html"
+  },
+  {
+    "revision": "d7204af37ed13c2a650b",
+    "url": "/form-app/static/css/main.033fa0d5.chunk.css"
+  },
+  {
+    "revision": "5f7a7a55bb2a2c22d7a4",
+    "url": "/form-app/static/js/2.5c292a0c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/form-app/static/js/2.5c292a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7204af37ed13c2a650b",
+    "url": "/form-app/static/js/main.e2484819.chunk.js"
+  },
+  {
+    "revision": "273ceb8563d39fe7199b",
+    "url": "/form-app/static/js/runtime-main.bbebb0d0.js"
+  }
+]);
